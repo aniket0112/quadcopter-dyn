0698b753daab7431f741c532883c14b75eaf543b
mishraiitm <mishra.nitt007@gmail.com>
2020-03-05 12:33:45 +0530

    lab

--- control_input.m
+++ control_input.m
@@ -2,13 +2,13 @@ function input = control_input(data,time,dt)
     if length(data) ~= length(time)
         error('Data and time series should be of equal length');
     else
-        t = [time(1):dt:time(end)];
+        t = [time(1):dt:time(end)+1];
         input = zeros(length(t),1);
         idx = 1;
         for i=1:length(time)-1
-            k = find(abs(t-round(time(i+1)))<0.1);
-            input(idx:k) = data(i);
-            idx = k;
+            k = find(abs(t-round(time(i+1)))<0.01);
+            input(idx:k(1)) = data(i);
+            idx = k(1);
         end
     end
 end
--- main.m
+++ main.m
@@ -1,18 +1,19 @@
-m = 1;
-ixx = 1;
-iyy = 1;
-izz = 2;
-l = 0.22;
-t = [0:0.1:60];
+m = 1.02;
+ixx = 2e-3;
+iyy = 2e-3;
+izz = 4e-3;
+l = 0.2;
+dt = 0.1;
+t = [0:dt:60];
 
 mavlink_time = motor.Time-motor.Time(1);
 [Y,M,D,hours,minutes,seconds] = datevec(mavlink_time);
 experiment_time = hours*3600+minutes*60+seconds;
-tu = experiment_time(78:100);
-tu = tu-tu(1);
-U = [control_input(t1(78:100),tu,0.1),control_input(t2(78:100),tu,0.1),control_input(t3(78:100),tu,0.1),control_input(t4(78:100),tu,0.1),...
-    control_input(tau1(78:100),tu,0.1),control_input(tau2(78:100),tu,0.1),control_input(tau3(78:100),tu,0.1),control_input(tau4(78:100),tu,0.1)];
-tu = [tu(1):0.1:tu(end)];
+tu_ = experiment_time(78:100);
+tu_ = tu_-tu_(1);
+U = [control_input(t1(78:100),tu_,dt),control_input(t2(78:100),tu_,dt),control_input(t3(78:100),tu_,dt),control_input(t4(78:100),tu_,dt),...
+    control_input(tau1(78:100),tu_,dt),control_input(tau2(78:100),tu_,dt),control_input(tau3(78:100),tu_,dt),control_input(tau4(78:100),tu_,dt)];
+tu = [tu_(1):dt:tu_(end)+1];
 % tu = [0:1:60];
 % U = [(unitstep(tu,-2,1)+unitstep(tu,2,10)+unitstep(tu,-11,30))',...
 %     (unitstep(tu,-2,1)+unitstep(tu,2,10)+unitstep(tu,-11,30))',...
