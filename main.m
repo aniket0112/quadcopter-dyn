148e1185dd754ae108b697b6d17d113e2e1c44da
mishraiitm <mishra.nitt007@gmail.com>
2020-03-05 19:01:25 +0530

    smooth data

--- main.m
+++ main.m
@@ -1,10 +1,9 @@
 m = 1.02;
-ixx = 2e-3;
-iyy = 2e-3;
-izz = 4e-3;
-l = 0.2;
+ixx = 2;
+iyy = 2;
+izz = 4;
+l = 0.22;
 dt = 0.1;
-t = [0:dt:60];
 
 mavlink_time = motor.Time-motor.Time(1);
 [Y,M,D,hours,minutes,seconds] = datevec(mavlink_time);
@@ -16,7 +15,7 @@ U = [control_input(t1(78:100),tu_,dt),control_input(t2(78:100),tu_,dt),control_i
 tu = [tu_(1):dt:tu_(end)+1];
 % tu = [0:1:60];
 % U = [(unitstep(tu,-2,1)+unitstep(tu,2,10)+unitstep(tu,-11,30))',...
-%     (unitstep(tu,-2,1)+unitstep(tu,2,10)+unitstep(tu,-11,30))',...
+%     (unitstep(tu,2,1)+unitstep(tu,-2,10)+unitstep(tu,11,30))',...
 %     (unitstep(tu,-2,1)+unitstep(tu,2,10)+unitstep(tu,-11,30))',...
 %     (unitstep(tu,-2,1)+unitstep(tu,2,10)+unitstep(tu,-11,30))',...
 %     zeros(length(tu),1),...
@@ -25,4 +24,4 @@ tu = [tu_(1):dt:tu_(end)+1];
 %     zeros(length(tu),1)];
 
 x0 = [0;0;0;0;0;0;0;0;0;0;0;-0.1];
-[tsol,ysol] = quadmodel(t,x0,U,ixx,iyy,izz,m,l,tu);
+[tsol,ysol] = quadmodel(tu,x0,U,ixx,iyy,izz,m,l,tu);
