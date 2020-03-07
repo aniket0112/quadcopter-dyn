1bbca4fc9d13f5af36ec5b567383a3159e3cc3ff
Aniket Sharmra <aniketkok10@gmail.com>
2020-03-07 22:32:11 +0530

    effed up all

--- main.m
+++ main.m
@@ -1,32 +1,10 @@
-m = 1.02;
-ixx = 2;
-iyy = 2;
-izz = 4;
-% ixx = 1.0446e-2;
-% iyy = 1.0714e-2;
-% izz = 1.3858e-2;
-l = 0.22;
-dt = 0.1;
-
-mavlink_time = motor.Time-motor.Time(1);
-[Y,M,D,hours,minutes,seconds] = datevec(mavlink_time);
-experiment_time = hours*3600+minutes*60+seconds;
-tu_ = experiment_time(73:148);
-tu_ = tu_-tu_(1);
-U = [control_input(t1(73:148),tu_,dt),control_input(t2(73:148),tu_,dt),control_input(t3(73:148),tu_,dt),control_input(t4(73:148),tu_,dt),...
-    control_input(tau1(73:148),tu_,dt),control_input(tau2(73:148),tu_,dt),control_input(tau3(73:148),tu_,dt),control_input(tau4(73:148),tu_,dt)];
-tu = [tu_(1):dt:tu_(end)+1];
-% tu = [0:1:60];
-% U = [(unitstep(tu,-5,0)+unitstep(tu,0,20)+unitstep(tu,0.01,22)+unitstep(tu,-0.01,24))',...
-%     (unitstep(tu,-5,0)+unitstep(tu,0.01,20)+unitstep(tu,-0.01,22)+unitstep(tu,0,24))',...
-%     (unitstep(tu,-5,0)+unitstep(tu,0,20)+unitstep(tu,0.01,22)+unitstep(tu,-0.01,24))',...
-%     (unitstep(tu,-5,0)+unitstep(tu,0.01,20)+unitstep(tu,-0.01,22)+unitstep(tu,0,24))',...
-%     zeros(length(tu),1),...
-%     zeros(length(tu),1),...
-%     zeros(length(tu),1),...
-%     zeros(length(tu),1)];
-
-x0 = [0;0;0;0;0;0;0;0;0;0;0;-0.1];
-[tsol,ysol] = quadmodel(tu,x0,U,ixx,iyy,izz,m,l,tu);
-%plot(U(:,1)); hold on; plot(U(:,2)); plot(U(:,3)); plot(U(:,4));
-%plot(-ysol(:,12))
+m = 2.6;
+ixx = 3.63;
+iyy = 3.25;
+izz = 5.51;
+l = 0.2;
+k =  2.82e-5;
+b = 7e-7;
+t = [0:60];
+x0 = [0;0;0;0;deg2rad(20);0;0;0;0;0;0;0;0;0];
+[tsol,ysol] = quadmodel(t,x0,ixx,iyy,izz,m,l,k,b,[500;500;500;500]);
--- quadmodel_control.m
+++ quadmodel_control.m
@@ -1,28 +1,19 @@
 % X = [u; v; w; phi; theta; psi; p; q; r; x; y; z]
 % T4->t1 T2->t2 T1->t3 T3->t4
-function [tsol,ysol] = quadmodel(t,x0,ixx,iyy,izz,m,l,k,b,rpm)
-    K =  [-1717 -1708 -672.3 634 -0.3 133.4 132.9;
-         -1613.2 1616.2 -613.1 581.1 0.5 125.5 -125.9;
-         1631.4 1623.1 638.8 602.4 -0.3 -126.8 -126.3;
-         1719.9 -1723 653.6 -619.5 0.5 -133.8 134.2];
+function [tsol,ysol] = quadmodel(t,x0,U,ixx,iyy,izz,m,l,tu)
     g = 9.8;
     [tsol,ysol] = ode45(@odefun,t,x0);
     function dX = odefun(t,X)
-        dX = zeros(14,1);
-        u = X(1); v = X(2); w = X(3); phi = X(4); theta = X(5); psi = X(6); p = X(7); q = X(8); r = X(9); x = X(10); y = X(11); z = X(12); ephi = X(13); etheta = X(14);
-        
-        X_ = [phi; theta; p; q; r; ephi; etheta];
-        deltaw = -K*X_;
-        rpm = rpm + deltaw;
-        t1 = -k*rpm(1)^2;
-        t2 = -k*rpm(2)^2;
-        t3 = -k*rpm(3)^2;
-        t4 = -k*rpm(4)^2;
-        tau1 = b*rpm(1)^2;
-        tau2 = b*rpm(2)^2;
-        tau3 = b*rpm(3)^2;
-        tau4 = b*rpm(4)^2;
-        
+        dX = zeros(12,1);
+        t1 = interp1(tu,U(:,1),t); 
+        t2 = interp1(tu,U(:,2),t); 
+        t3 = interp1(tu,U(:,3),t); 
+        t4 = interp1(tu,U(:,4),t);
+        tau1 = interp1(tu,U(:,5),t); 
+        tau2 = interp1(tu,U(:,6),t); 
+        tau3 = interp1(tu,U(:,7),t); 
+        tau4 = interp1(tu,U(:,8),t);
+        u = X(1); v = X(2); w = X(3); phi = X(4); theta = X(5); psi = X(6); p = X(7); q = X(8); r = X(9); x = X(10); y = X(11); z = X(12);
         dX(1) = r*v-q*w-g*sin(theta);
         dX(2) = -r*u+p*w+g*sin(phi)*cos(theta);
         dX(3) = q*u-p*v+g*cos(phi)*cos(theta)+(t1+t2+t3+t4)/m;
@@ -35,7 +26,5 @@ function [tsol,ysol] = quadmodel(t,x0,ixx,iyy,izz,m,l,k,b,rpm)
         dX(10) = u*cos(theta)*cos(psi)+v*(-cos(phi)*sin(psi)+sin(phi)*sin(theta)*cos(psi))+w*(sin(phi)*sin(psi)+cos(phi)*sin(theta)*cos(psi));
         dX(11) = u*cos(theta)*sin(psi)+v*(cos(phi)*cos(psi)+sin(phi)*sin(theta)*sin(psi))+w*(-sin(phi)*cos(psi)+cos(phi)*sin(theta)*sin(psi));
         dX(12) = -u*sin(theta)+v*sin(phi)*cos(theta)+w*cos(phi)*cos(theta);
-        dX(13) = -phi;
-        dX(14) = -theta;
    end
 end
