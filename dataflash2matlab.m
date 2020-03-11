72ddd49ad49b7c04e5ae2475dfa56d07f30fa036
mishraiitm <mishra.nitt007@gmail.com>
2020-03-11 16:53:33 +0530

    hover.bin

---
+++ dataflash2matlab.m
@@ -0,0 +1,30 @@
+log = Ardupilog('hover.bin');
+close all
+MOTOR_VOLTAGE = 12;
+MOTOR_KV = 1000;
+PWM_TPERIOD = 1000;
+DUTYCYCLE2RADS = MOTOR_VOLTAGE/PWM_TPERIOD*MOTOR_KV*2*3.14/60; 
+k = 1.9794e-7 * 3600/(2*3.14)^2;
+b = 6.5124e-9* 3600/(2*3.14)^2;
+
+u1 = (log.RCOU.C1-1000)*DUTYCYCLE2RADS;
+u2 = (log.RCOU.C2-1000)*DUTYCYCLE2RADS;
+u3 = (log.RCOU.C3-1000)*DUTYCYCLE2RADS;
+u4 = (log.RCOU.C4-1000)*DUTYCYCLE2RADS;
+t = log.RCOU.TimeS;
+t = t - t(1);
+log_t1 = -k*u1.^2;
+log_t2 = -k*u2.^2;
+log_t3 = -k*u3.^2;
+log_t4 = -k*u4.^2;
+log_tau1 = b*u1.^2;
+log_tau2 = b*u2.^2;
+log_tau3 = b*u3.^2;
+log_tau4 = b*u4.^2;
+
+
+lat = log.AHR2.Lat;
+lng = log.AHR2.Lng;
+alt = log.AHR2.Alt;
+
+
